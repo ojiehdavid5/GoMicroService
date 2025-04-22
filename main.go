4a0aee5562f51d7a61501772d40ce2d53ea39416
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-22 10:55:25 +0100

    DONE

--- main.go
+++ main.go
@@ -37,63 +37,68 @@ func main() {
 
 	// fmt.Println("Hello, World!")
 	// fmt.Println("This is a test.")
-	port := 8080
-	http.HandleFunc("/helloworld", helloWorldHandler)
-	// servering a static file to the broswer as response
-	http.Handle("/images", http.FileServer(http.Dir("./images")))
+	// port := 8080
+	// http.HandleFunc("/helloworld", helloWorldHandler)
+	// // servering a static file to the broswer as response
+	// http.Handle("/images", http.FileServer(http.Dir("./images")))
 
 
-	log.Printf("Server starting on port %v\n", port)
-	http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
-	log.Fatal()
+	// log.Printf("Server starting on port %v\n", port)
+	// http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+	// log.Fatal()
 
 	//Reading and writing json
 
 	//“Marshalling Go structs to JSON”
 
+
+
+
+	//CONTEXT ARE USED FOR CONTROLLING TIMEOUTS, CANCELLATIONS, AND DEADLINES AN PASSING DATA BETWEEN GOROUTINES
+
 }
 
 //starting a server
 
-type helloWorldResponse struct {
-	Message string `json:"Message"`
-}
+// type helloWorldResponse struct {
+// 	Message string `json:"Message"`
+// }
 
-type helloWorldRequest struct {
-	Name string `json:"name"`
-}
+// type helloWorldRequest struct {
+// 	Name string `json:"name"`
+// }
 
-func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	// 	body,err:=io.ReadAll(r.Body)
-	// 	if err!=nil{
-	// 		http.Error(w, "Bad request", http.StatusBadRequest)
-	// return
-	// 	}
+// func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+// 	// 	body,err:=io.ReadAll(r.Body)
+// 	// 	if err!=nil{
+// 	// 		http.Error(w, "Bad request", http.StatusBadRequest)
+// 	// return
+// 	// 	}
 
-	var request helloWorldRequest
-	fmt.Println(request)
+// 	var request helloWorldRequest
+// 	fmt.Println(request)
 
 	// err = json.Unmarshal(body,&request)
 
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&request)
-	fmt.Println(request)
+	// decoder := json.NewDecoder(r.Body)
+	// err := decoder.Decode(&request)
+	// fmt.Println(request)
 
-	if err != nil {
-		http.Error(w, "Bad request", http.StatusBadRequest)
-		return
+	// if err != nil {
+	// 	http.Error(w, "Bad request", http.StatusBadRequest)
+	// 	return
 
-	}
+	// }
 
-	// fmt.Fprint(w, "Hello World\n")
-	response := helloWorldResponse{Message: "Hello" + request.Name}
+	// // fmt.Fprint(w, "Hello World\n")
+	// response := helloWorldResponse{Message: "Hello" + request.Name}
 
-	if err := json.NewEncoder(w).Encode(response); err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-	// encoder.Encode(response)
-	fmt.Println(response)
+	// if err := json.NewEncoder(w).Encode(response); err != nil {
+	// 	http.Error(w, "Internal server error", http.StatusInternalServerError)
+	// 	return
+	// }
+	// // encoder.Encode(response)
+	// fmt.Println(response)
 
 	//marshal
 	// data,err:=json.Marshal(response)
@@ -101,7 +106,7 @@ func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	// 	panic("oops");
 
 	
-}
+//}
 
 // fmt.Fprint(w,string(data))
 // fmt.Print(string(data))
